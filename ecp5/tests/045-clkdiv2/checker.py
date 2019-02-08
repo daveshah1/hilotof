4ee4e53fadd30c7b5f2abeb8b5f2f1aeac129f90
David Shah <dave@ds0.me>
2019-02-08 19:25:30 +0000

    Add CLKDIV tests
    Signed-off-by: David Shah <dave@ds0.me>

--- ecp5/tests/045-clkdiv2/checker.py
+++ ecp5/tests/045-clkdiv2/checker.py
@@ -2,6 +2,6 @@ from interface import DUTInterface
 
 def main():
 	with DUTInterface() as dut:
-		dut.assert_freq(2.4e6, 0.1)
+		dut.assert_freq(50e6, 0.001)
 if __name__ == "__main__":
 	main()
---
+++ ecp5/tests/046-clkdiv3p5/dut.v
@@ -0,0 +1,20 @@
+module dut(
+	input clock, reset,
+	input din_valid,
+	input [31:0] din,
+	input dout_ready,
+	output dout_valid,
+	output [31:0] dout
+);
+	wire cdiv;
+	CLKDIVF #(.DIV("3.5")) div_i(.CLKI(clock), .RST(1'b0), .ALIGNWD(1'b0), .CDIVX(cdiv));
+
+	freq_counter fc_i (
+		.sys_clock(clock),
+		.probe(cdiv),
+		.freq(dout[23:0])
+	);
+
+	assign dout[31:24] = 8'h00;
+	assign dout_valid = 1'b1;
+endmodule
